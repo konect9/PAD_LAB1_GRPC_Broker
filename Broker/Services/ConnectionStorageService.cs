0de4b0a8a770f4ff38b7b77f5e66d01d221e713b
konect9 <sorinbulgari@gmail.com>
2020-09-28 02:47:33 +0300

    first commit

---
+++ Broker/Services/ConnectionStorageService.cs
@@ -0,0 +1,47 @@
+﻿using Broker.Models;
+using Broker.Services.Interfaces;
+using Microsoft.AspNetCore.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Broker.Services
+{
+    public class ConnectionStorageService : IConnectionStorageService
+    {
+        private readonly List<Connection> _connections;
+        private readonly object _locker;
+
+        public ConnectionStorageService()
+        {
+            _connections = new List<Connection>();
+            _locker = new object();
+        }
+
+        public void Add(Connection connection)
+        {
+            lock (_locker)
+            {
+                _connections.Add(connection);
+            }
+        }
+
+        public IList<Connection> GetConnectionsByTopic(string topic)
+        {
+            lock (_locker)
+            {
+                var filteredConnections = _connections.Where(x => x.Topic == topic).ToList();
+                return filteredConnections;
+            }
+        }
+
+        public void Remove(string address)
+        {
+            lock (_locker)
+            {
+                _connections.RemoveAll(x => x.Address == address);
+            }
+        }
+    }
+}
